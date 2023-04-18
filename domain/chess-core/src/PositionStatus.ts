36671fdbb6c027e94e53996e07dcf767e89b13eb
artemis prime <artemisprimedev@gmail.com>
2023-04-18 10:58:48 -0400

    minor

--- domain/chess-core/src/PositionStatus.ts
+++ domain/chess-core/src/PositionStatus.ts
@@ -1,14 +1,16 @@
 import type Action from './Action'
   
-  // Should be enough for the UI to give feedback based on these values.
-  // (TODO: Consider making this an array of possibly coincident statuses)
+  // Possible 'status' a position could have in the current dnd
+  // or current check. 
+  // These are not used by the domain core, but are a convenience 
+  // for apps so the UI can give feedback.
 type PositionStatus = 
-   Action |               // current square resolves to the Action
-  'origin' |              // origin of the drag
-  'invalid' |             // over this square, but no valid move
-  'castleRookFrom' |    // action is 'castle' and this square is the rook's origin in the castle
-  'castleRookTo' |      // action is 'castle' and this square is the rook's destination in the castle
-  'none' |                // not under in the current drag or involved
+   Action |           // current square resolves to the Action
+  'origin' |          // origin of the drag
+  'invalid' |         // over this square, but no valid move
+  'castleRookFrom' |  // action is 'castle' and this square is the rook's origin
+  'castleRookTo' |    // action is 'castle' and this square is the rook's destination
+  'none' |            // not involved in the current drag
   'kingInCheck' | 
   'inCheckFrom'
 
