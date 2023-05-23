11bf020a7b1b42ebc8cb23ce1f860fc3c4014a84
artemis prime <artemisprimedev@gmail.com>
2023-05-23 17:13:04 -0400

    minor

--- apps/chess-rn/src/primatives/GhostButton.tsx
+++ apps/chess-rn/src/primatives/GhostButton.tsx
@@ -9,75 +9,62 @@ import { styled, typography, type CSS } from '~/style'
 
 import ButtonShell, {type ButtonViewProps} from './ButtonShell'
 
-const GhostButtonBG = styled(View, 
-  {
-    borderRadius: '$menuRadius',
-    backgroundColor: 'transparent',
-    variants: {
-      state: {
-        disabled: {},
-        pressed: {},
-        normal: {}
-      },
-      chalkboard: {
-        true: {}
-      },
-      menu: {
-        true: { }
-      },
-    },
-    compoundVariants: [
-      {
-        chalkboard: true,
-        state: 'pressed',
-        css: {
-          backgroundColor: '$chalkboardButtonPressedBG',
-        }
-      },
-      {
-        menu: true,
-        state: 'pressed',
-        css: {
-          backgroundColor: '$menuBGColorPressed',
-        }
-      },
-    ]
-  }
-)
+const GhostButtonBG = styled(View, {
 
-const GhostButtonText = styled(Text, 
-  {
-    variants: {
-      state: {
-        disabled: {},
-        pressed: {},
-        normal: {}
-      },
-      chalkboard: {
-        true: { ...typography.chalkboard.normal }
-      },
-      menu: {
-        true: { ...typography.menu.sectionTitle }
-      },
+  borderRadius: '$menuRadius',
+  backgroundColor: 'transparent',
+
+  variants: {
+    state: {
+      disabled: {},
+      pressed: {},
+      normal: {}
     },
-    compoundVariants: [
-      {
-        chalkboard: true,
-        state: 'disabled',
-        css: {
-          color: '$chalkboardTextColorDisabled',
-        }
-      },
-      {
-        menu: true,
-        state: 'disabled',
-        css: {
-          color: '$menuTextColorDisabled',
-        }
-      },
-    ]
-  }
-)
+    chalkboard: { true: {} },
+    menu: { true: {} },
+  },
+  compoundVariants: [
+    {
+      chalkboard: true,
+      state: 'pressed',
+      css: { backgroundColor: '$chalkboardButtonPressedBG' }
+    },
+    {
+      menu: true,
+      state: 'pressed',
+      css: { backgroundColor: '$menuBGColorPressed' }
+    },
+  ]
+})
+
+const GhostButtonText = styled(Text, {
+
+  variants: {
+    state: {
+      disabled: {},
+      pressed: {},
+      normal: {}
+    },
+    chalkboard: {
+      true: { ...typography.chalkboard.normal }
+    },
+    menu: {
+      true: { ...typography.menu.sectionTitle }
+    },
+  },
+  compoundVariants: [
+    {
+      chalkboard: true,
+      state: 'disabled',
+      css: { color: '$chalkboardTextColorDisabled' }
+    },
+    {
+      menu: true,
+      state: 'disabled',
+      css: { color: '$menuTextColorDisabled' }
+    },
+  ]
+})
 
 const GhostButtonView: React.FC<{
   menu?: boolean
